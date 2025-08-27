1139ae04fa60d310ec882485740ede16ce680686
diogo-rp-menezes <diogorpm@gmail.com>
2025-08-27 01:46:49 -0300

    Correção: remover avisos de versão de pom.xml e adicionar serialVersionUID às exceções
    - Versões explícitas removidas de pom.xml para usar versões gerenciadas do Spring Boot
    
    - Adição de serialVersionUID a NotFoundException e DuplicateException
    
    - Limpeza da configuração do Maven para eliminar avisos de build

--- src/main/java/br/com/aegispatrimonio/exception/DuplicateException.java
+++ src/main/java/br/com/aegispatrimonio/exception/DuplicateException.java
@@ -1,7 +1,13 @@
 package br.com.aegispatrimonio.exception;
 
 public class DuplicateException extends RuntimeException {
+    private static final long serialVersionUID = 2L; // ← ADICIONE ESTA LINHA
+    
     public DuplicateException(String message) {
         super(message);
+    }
+    
+    public DuplicateException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
