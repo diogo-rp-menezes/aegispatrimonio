a0df384590ee74abd2700d83cbed9daa4e65e8b2
diogo-rp-menezes <diogorpm@gmail.com>
2025-09-01 01:04:29 -0300

    feat: implementação completa de todos os services com DTOs
    - Finalizado FilialService com suporte a DTOs
    
    - Completado todos os services para trabalhar com DTOs
    
    - Implementados todos os DTOs de request e response
    
    - Validações com Bean Validation consolidadas
    
    - Projeto compilando com sucesso
    
    - Estrutura completa de services finalizada

---
+++ src/main/java/br/com/aegispatrimonio/dto/request/FilialRequestDTO.java
@@ -0,0 +1,21 @@
+package br.com.aegispatrimonio.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FilialRequestDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 100, message = "Nome deve ter no máximo 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Código é obrigatório")
+    @Size(max = 10, message = "Código deve ter no máximo 10 caracteres")
+    private String codigo;
+}
